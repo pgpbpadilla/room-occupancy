0dd917e1cd1841f7d20e56cccce59e44dac08628
Pablo Padilla <pgpb.padilla@gmail.com>
2018-10-18 16:37:36 +0200

    new: dev: Springboot CLI generated files

---
+++ src/main/java/com/smarhost/occupancy/RoomOccupancy.java
@@ -0,0 +1,12 @@
+package com.smarhost.occupancy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomOccupancy {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomOccupancy.class, args);
+	}
+}
